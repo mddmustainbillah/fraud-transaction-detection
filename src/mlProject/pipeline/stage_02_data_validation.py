f245f18024b200419afff6f3925e0492c018f74c
Mustain Billah <mustainbillah017@gmail.com>
2023-12-28 11:48:45 +0600

    data transformation added

---
+++ src/mlProject/pipeline/stage_02_data_validation.py
@@ -0,0 +1,16 @@
+from src.mlProject.config.configuration import ConfigurationManager
+from src.mlProject.components.data_validation import DataValiadtion
+from mlProject import logger
+
+
+STAGE_NAME = "Data Validation stage"
+
+class DataValidationTrainingPipeline:
+    def __init__(self):
+        pass
+
+    def main(self):
+        config = ConfigurationManager()
+        data_validation_config = config.get_data_validation_config()
+        data_validation = DataValiadtion(config=data_validation_config)
+        data_validation.validate_all_columns()
